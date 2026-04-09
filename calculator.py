8c1526268797d3f6bef3a1b756336fcbcdaf8494
dalynbrown <dalynbrown@ufl.edu>
2026-04-09 13:22:43 -0400

    new functions

--- calculator.py
+++ calculator.py
@@ -11,8 +11,8 @@ def multiply(a, b):
 
 def logarithm(a, b):
    if a <= 0 or a == 1 or b <= 0:
-        raise ValueError("Invalid input for logarithm")
-    return math.log(b, a)
+       raise ValueError("Invalid input for logarithm")
+   return math.log(b, a)
 
 def exponent(a, b):
     return a**b
