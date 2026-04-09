286106180fbe025344938bfd6883d1d1b56c7b92
Salma Ameziane <salmaameziane@Salmas-MacBook-Air.local>
2026-04-09 13:08:04 -0400

    added unit tests

--- test_calculator.py
+++ test_calculator.py
@@ -17,6 +17,12 @@ class TestCalculator(unittest.TestCase):
     # def test_divide(self): # 3 assertions
     #     fill in code
     # ##########################
+    def test_multiply(self):
+        self.assertEqual(multiply(2, 3), 6)
+
+    def test_divide(self):
+        self.assertEqual(divide(2, 10), 5)
+
 
     ######## Partner 2
     # def test_divide_by_zero(self): # 1 assertion
@@ -34,14 +40,15 @@ class TestCalculator(unittest.TestCase):
     # ##########################
     
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    def test_log_invalid_argument(self):
+            with self.assertRaises(ValueError):
+                logarithm(-1, 10)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_hypotenuse(self):
+        self.assertAlmostEqual(hypotenuse(3, 4), 5)
+
+    def test_sqrt(self):
+        self.assertAlmostEqual(square_root(9), 3)
 
     # def test_sqrt(self): # 3 assertions
     #     # Test for invalid argument, example:
